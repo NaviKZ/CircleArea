cf92bdfd00b6a4c857ca62e6c7c9d80a20ab42e3
Ivan Dyakov <dyakovivan@hotmail.com>
2024-07-18 18:46:31 +0200

    Добавьте файлы проекта.

---
+++ CircleArea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CircleArea
+{
+    internal class CircleArea
+    {
+        static void Main(string[] args)
+        {
+            double radius = 20; //TODO: Осуществить ввод с консоли
+            double circleArea = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine("Площадь круга равна: {0:f3}", circleArea);
+
+            Console.ReadKey();
+        }
+    }
+}
